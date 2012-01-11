bc637e8daae0c4c9be8ceb2f65796b6a856514c3
jas- <jason.gerfen@gmail.com>
2012-01-11 08:35:15 -0700

    Additional changes

--- jQuery.AJAX/proxy.php
+++ jQuery.AJAX/proxy.php
@@ -1,66 +1,51 @@
 <?php
 
-/* CSRF helper (this must match the appID from client) */
-$appID = 'jQuery.AJAX';
+/* load libraries first */
+if (!file_exists('classes/class.libraries.php')){
+ exit('Required libraries were not found. Please reinstall jQuery.AJAX distribution.');
+}
+include_once 'classes/class.libraries.php';
+
+/* load up the libraries */
+$libs = new libraries;
+
+/* ensure sessions are started */
+session_start();
+
+/* Does a GUID exist for this machine? */
+if (!isset($_SESSION[$libs->_getRealIPv4()])){
+ $_SESSION[$libs->_getRealIPv4()]=$libs->_uuid();
+}
+
+/* regenerate the session ID to help prevent replay' attacks */
+session_regenerate_id(true);
 
 /* verify an XMLHttpRequest was made */
 if (strcmp($_SERVER['HTTP_X_REQUESTED_WITH'], 'XMLHttpRequest')!==0){
- exit(JSONencode(array('error'=>'An XMLHttpRequest was not made')));
+ exit($libs->JSONencode(array('error'=>'An XMLHttpRequest was not made')));
 }
 
 /* verify associated X-ALT-Header value */
-if (strcmp($_SERVER['HTTP_X_ALT_REFERER'], $appID)!==0){
- exit(JSONencode(array('error'=>'The X-Alt-Referer information recieved is invalid')));
+if (strcmp($_SERVER['HTTP_X_ALT_REFERER'], $_SESSION[$libs->_getRealIPv4()])!==0){
+ exit($libs->JSONencode(array('error'=>'The X-Alt-Referer information recieved is invalid')));
 }
 
 /* verify associated Content-MD5 header value */
 if (!empty($_POST)){
- if (strcmp(base64_decode($_SERVER['HTTP_CONTENT_MD5']), md5(_serialize($_POST)))!==0){
-  exit(JSONencode(array('error'=>'The Content-MD5 value is incorrect. Checksum failed on submitted form data')));
+ if (strcmp(base64_decode($_SERVER['HTTP_CONTENT_MD5']), md5($libs->_serialize($_POST)))!==0){
+  exit($libs->JSONencode(array('error'=>'The Content-MD5 value is incorrect. Checksum failed on submitted form data')));
  }
 } else {
- if (strcmp(base64_decode($_SERVER['HTTP_CONTENT_MD5']), md5($appID))!==0){
-  exit(JSONencode(array('error'=>'The Content-MD5 value is incorrect. Checksum failed on configured CSRF')));
+ if (strcmp(base64_decode($_SERVER['HTTP_CONTENT_MD5']), md5($_SESSION[$libs->_getRealIPv4()]))!==0){
+  exit($libs->JSONencode(array('error'=>'The Content-MD5 value is incorrect. Checksum failed on configured CSRF')));
  }
 }
 
-exit(JSONencode(array('success'=>'All validation checks passed')));
+exit($libs->JSONencode(array('success'=>'All validation checks passed',
+                             'details'=>array('Remote address'=>$libs->_getRealIPv4(),
+                                              'Session ID'=>$_SESSION[$libs->_getRealIPv4()],
+                                              'X-Alt-Referer header'=>getenv('HTTP_X_ALT_REFERER'),
+                                              'Content-MD5 header'=>getenv('HTTP_CONTENT_MD5'),
+                                              'Serialized POST data'=>$libs->_serialize($_POST)))));
 
-function _serialize($array)
-{
- if (count($array)>0){
-  $x = '';
-  foreach($array as $key => $value){
-   $x .= $key.'='.$value.'&';
-  }
-  $x = substr($x, 0, -1);
- }
- return (strlen($x)>0) ? $x : false;
-}
-
-function JSONencode($array){
- if (!function_exists('json_encode')) {
-  return arr2json($array);
- } else {
-  return json_encode($array);
- }
-}
-
-function arr2json($array)
-{
- if (is_array($array)) {
-  foreach($array as $key => $value) $json[] = $key . ':' . self::php2js($value);
-  if(count($json)>0) return '{'.implode(',',$json).'}';
-  else return '';
- }
-}
-
-function php2js($value)
-{
- if(is_array($value)) return self::arr2json($val);
- if(is_string($value)) return '"'.addslashes($value).'"';
- if(is_bool($value)) return 'Boolean('.(int) $value.')';
- if(is_null($value)) return '""';
- return $value;
-}
 ?>
