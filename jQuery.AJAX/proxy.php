e7c2d36d84a633a3c98a77f1ef732b7fd86b4e46
jas- <jason.gerfen@gmail.com>
2012-01-11 09:45:39 -0700

    Simplified and expanded details of validation checks

--- jQuery.AJAX/proxy.php
+++ jQuery.AJAX/proxy.php
@@ -20,32 +20,39 @@ if (!isset($_SESSION[$libs->_getRealIPv4()])){
 /* regenerate the session ID to help prevent replay' attacks */
 session_regenerate_id(true);
 
+/* create array of details that are used in the next steps */
+$details = array('details'=>array('Remote address'=>$libs->_getRealIPv4(),
+                                  'Session ID'=>$_SESSION[$libs->_getRealIPv4()],
+                                  'X-Alt-Referer header'=>getenv('HTTP_X_ALT_REFERER'),
+                                  'Content-MD5 header'=>getenv('HTTP_CONTENT_MD5'),
+                                  'Serialized POST data'=>$libs->_serialize($_POST)));
+
 /* verify an XMLHttpRequest was made */
 if (strcmp($_SERVER['HTTP_X_REQUESTED_WITH'], 'XMLHttpRequest')!==0){
- exit($libs->JSONencode(array('error'=>'An XMLHttpRequest was not made')));
+ exit($libs->JSONencode(array('error'=>'An XMLHttpRequest was not made',
+                              'details'=>$details)));
 }
 
 /* verify associated X-ALT-Header value */
 if (strcmp($_SERVER['HTTP_X_ALT_REFERER'], $_SESSION[$libs->_getRealIPv4()])!==0){
- exit($libs->JSONencode(array('error'=>'The X-Alt-Referer information recieved is invalid')));
+ exit($libs->JSONencode(array('error'=>'The X-Alt-Referer information recieved is invalid',
+                              'details'=>$details)));
 }
 
 /* verify associated Content-MD5 header value */
 if (!empty($_POST)){
  if (strcmp(base64_decode($_SERVER['HTTP_CONTENT_MD5']), md5($libs->_serialize($_POST)))!==0){
-  exit($libs->JSONencode(array('error'=>'The Content-MD5 value is incorrect. Checksum failed on submitted form data')));
+  exit($libs->JSONencode(array('error'=>'The Content-MD5 value is incorrect. Checksum failed on submitted form data',
+                               'details'=>$details)));
  }
 } else {
  if (strcmp(base64_decode($_SERVER['HTTP_CONTENT_MD5']), md5($_SESSION[$libs->_getRealIPv4()]))!==0){
-  exit($libs->JSONencode(array('error'=>'The Content-MD5 value is incorrect. Checksum failed on configured CSRF')));
+  exit($libs->JSONencode(array('error'=>'The Content-MD5 value is incorrect. Checksum failed on configured CSRF',
+                               'details'=>$details)));
  }
 }
 
 exit($libs->JSONencode(array('success'=>'All validation checks passed',
-                             'details'=>array('Remote address'=>$libs->_getRealIPv4(),
-                                              'Session ID'=>$_SESSION[$libs->_getRealIPv4()],
-                                              'X-Alt-Referer header'=>getenv('HTTP_X_ALT_REFERER'),
-                                              'Content-MD5 header'=>getenv('HTTP_CONTENT_MD5'),
-                                              'Serialized POST data'=>$libs->_serialize($_POST)))));
+                             'details'=>$details)));
 
 ?>
