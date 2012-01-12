8e5f4e118bdac1c2e004402fbc02445c783d6c2f
jas- <jason.gerfen@gmail.com>
2012-01-12 08:08:51 -0700

    Added some notes on why the header options get set by the client and best practices

--- jQuery.AJAX/proxy.php
+++ jQuery.AJAX/proxy.php
@@ -17,7 +17,14 @@ if (!isset($_SESSION[$libs->_getRealIPv4()])){
  $_SESSION[$libs->_getRealIPv4()]=$libs->_uuid();
 }
 
-/* setup headers if the client set them */
+/*
+ * setup headers if the client set them. we do this just in case
+ * the installed web server is not configured to enable these by default.
+ * however, because they are set by the client the chance for modification
+ * during a MITM attack is greater. the server should set these which
+ * in this example the ../index.php script does and here we just mimic
+ * what the client requests.
+ */
 header('X-Alt-Referer: '.getenv('HTTP_X_ALT_REFERER'));
 header('X-Forwarded-Proto: '.getenv('HTTP_X_FORWARDED_PROTO'));
 header('X-Frame-Options: '.getenv('HTTP_X_FRAME_OPTIONS'));
