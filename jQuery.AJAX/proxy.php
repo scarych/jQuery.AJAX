b16f07f7cb19848f7919b064211a43e85d0c3c41
jas- <jason.gerfen@gmail.com>
2012-01-12 08:03:13 -0700

    Sets security headers to clients specifications

--- jQuery.AJAX/proxy.php
+++ jQuery.AJAX/proxy.php
@@ -17,6 +17,12 @@ if (!isset($_SESSION[$libs->_getRealIPv4()])){
  $_SESSION[$libs->_getRealIPv4()]=$libs->_uuid();
 }
 
+/* setup headers if the client set them */
+header('X-Alt-Referer: '.getenv('HTTP_X_ALT_REFERER'));
+header('X-Forwarded-Proto: '.getenv('HTTP_X_FORWARDED_PROTO'));
+header('X-Frame-Options: '.getenv('HTTP_X_FRAME_OPTIONS'));
+header('X-XSS-Protecton: '.getenv('HTTP_X_XSS_PROTECTION'));
+
 /* regenerate the session ID to help prevent replay' attacks */
 session_regenerate_id(true);
 
@@ -28,25 +34,25 @@ $details = array('Remote address'=>$libs->_getRealIPv4(),
                  'Serialized POST data'=>$libs->_serialize($_POST));
 
 /* verify an XMLHttpRequest was made */
-if (strcmp($_SERVER['HTTP_X_REQUESTED_WITH'], 'XMLHttpRequest')!==0){
+if (strcmp(getenv('HTTP_X_REQUESTED_WITH'), 'XMLHttpRequest')!==0){
  exit($libs->JSONencode(array('error'=>'An XMLHttpRequest was not made',
                               'details'=>$details)));
 }
 
 /* verify associated X-ALT-Header value */
-if (strcmp($_SERVER['HTTP_X_ALT_REFERER'], $_SESSION[$libs->_getRealIPv4()])!==0){
+if (strcmp(getenv('HTTP_X_ALT_REFERER'), $_SESSION[$libs->_getRealIPv4()])!==0){
  exit($libs->JSONencode(array('error'=>'The X-Alt-Referer information recieved is invalid',
                               'details'=>$details)));
 }
 
 /* verify associated Content-MD5 header value with serialized $_POST data */
 if (!empty($_POST)){
- if (strcmp(base64_decode($_SERVER['HTTP_CONTENT_MD5']), md5($libs->_serialize($_POST)))!==0){
+ if (strcmp(base64_decode(getenv('HTTP_CONTENT_MD5')), md5($libs->_serialize($_POST)))!==0){
   exit($libs->JSONencode(array('error'=>'The Content-MD5 value is incorrect. Checksum failed on submitted form data',
                                'details'=>$details)));
  }
 } else {
- if (strcmp(base64_decode($_SERVER['HTTP_CONTENT_MD5']), md5($_SESSION[$libs->_getRealIPv4()]))!==0){
+ if (strcmp(base64_decode(getenv('HTTP_CONTENT_MD5')), md5($_SESSION[$libs->_getRealIPv4()]))!==0){
   exit($libs->JSONencode(array('error'=>'The Content-MD5 value is incorrect. Checksum failed on configured CSRF',
                                'details'=>$details)));
  }
