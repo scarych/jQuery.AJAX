2e24eb355876c0f57573401683fd5e8545de9ffa
jas- <jason.gerfen@gmail.com>
2012-01-30 05:15:00 -0700

    Mon Jan 30 05:15:00 MST 2012

--- jQuery.AJAX/proxy.php
+++ jQuery.AJAX/proxy.php
@@ -33,42 +33,10 @@ header('X-XSS-Protection: '.getenv('HTTP_X_XSS_PROTECTION'));
 /* regenerate the session ID to help prevent replay' attacks */
 session_regenerate_id(true);
 
-/* create array of details that are used in the next steps */
-$details = array('Remote address'=>$libs->_getRealIPv4(),
-                 'Session ID'=>$_SESSION[$libs->_getRealIPv4()],
-                 'X-Alt-Referer header'=>getenv('HTTP_X_ALT_REFERER'),
-                 'Content-MD5 header'=>getenv('HTTP_CONTENT_MD5'),
-                 'X-XSS-Protection'=>getenv('HTTP_X_XSS_PROTECTION'),
-                 'X-Frame-Options'=>getenv('HTTP_X_FRAME_OPTIONS'),
-                 'X-Forwarded-Proto'=>getenv('HTTP_X_FORWARDED_PROTO'),
-                 'Serialized POST data'=>$libs->_serialize($_POST));
-
-/* verify an XMLHttpRequest was made */
-if (strcmp(getenv('HTTP_X_REQUESTED_WITH'), 'XMLHttpRequest')!==0){
- exit($libs->JSONencode(array('error'=>'An XMLHttpRequest was not made',
-                              'details'=>$details)));
+/* load & initialize the XMLHttpRequest proxy processor */
+if (!file_exists('classes/class.ajax.php')){
+ exit('Required libraries were not found. Please reinstall jQuery.AJAX distribution.');
 }
-
-/* verify associated X-ALT-Header value */
-if (strcmp(getenv('HTTP_X_ALT_REFERER'), $_SESSION[$libs->_getRealIPv4()])!==0){
- exit($libs->JSONencode(array('error'=>'The X-Alt-Referer information recieved is invalid',
-                              'details'=>$details)));
-}
-
-/* verify associated Content-MD5 header value with serialized $_POST data */
-if (!empty($_POST)){
- if (strcmp(base64_decode(getenv('HTTP_CONTENT_MD5')), md5($libs->_serialize($_POST)))!==0){
-  exit($libs->JSONencode(array('error'=>'The Content-MD5 value is incorrect. Checksum failed on submitted form data',
-                               'details'=>$details)));
- }
-} else {
- if (strcmp(base64_decode(getenv('HTTP_CONTENT_MD5')), md5($_SESSION[$libs->_getRealIPv4()]))!==0){
-  exit($libs->JSONencode(array('error'=>'The Content-MD5 value is incorrect. Checksum failed on configured CSRF',
-                               'details'=>$details)));
- }
-}
-
-exit($libs->JSONencode(array('success'=>'All validation checks passed',
-                             'details'=>$details)));
-
+include_once 'classes/class.ajax.php';
+$a = new ajax;
 ?>
